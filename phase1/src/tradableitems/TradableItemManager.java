686f4d5951079d0ccdd16586682b9d38bdf51eb3
truanov1 <ilancito7@gmail.com>
2020-06-19 18:55:22 -0400

    Finished TradableItemManager (error handling may be changed later).

--- phase1/src/tradableitems/TradableItemManager.java
+++ phase1/src/tradableitems/TradableItemManager.java
@@ -6,7 +6,20 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class TradableItemManager extends Manager<TradableItem> implements Serializable {
+
     public TradableItemManager(String filePath) throws IOException {
         super(filePath);
+    }
+
+    public int addItem(String name, String description){
+        TradableItem item = new TradableItem(name, description);
+        try{
+            update(item);
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println("Class TradableItem is not found.");
+            return -1;
+        }
+        return item.getId();
     }
 }
