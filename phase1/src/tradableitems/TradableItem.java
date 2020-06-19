33a2aba6b8f9fffefcb45f6d2aa6c04e0d773708
truanov1 <ilancito7@gmail.com>
2020-06-19 18:30:18 -0400

    Finished implementation.

--- phase1/src/tradableitems/TradableItem.java
+++ phase1/src/tradableitems/TradableItem.java
@@ -5,4 +5,18 @@ import main.DatabaseItem;
 import java.io.Serializable;
 
 public class TradableItem extends DatabaseItem implements Serializable {
+
+    private String name;
+    private String description;
+
+    /**
+     * Constructs a tradable item.
+     * @param name The name of the TradableItem
+     * @param description The description of the TradableItem
+     */
+    public TradableItem(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
 }
