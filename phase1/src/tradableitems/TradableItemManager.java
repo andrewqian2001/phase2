1f95ef24b0d0618af43379c25ab7ab0ecc9a1dfd
James <smurf.lee@mail.utoronto.ca>
2020-06-20 00:47:35 -0400

    TradeManager completed

--- phase1/src/tradableitems/TradableItemManager.java
+++ phase1/src/tradableitems/TradableItemManager.java
@@ -6,28 +6,38 @@ import main.Manager;
 import java.io.IOException;
 import java.io.Serializable;
 
+/**
+ * Used to manage and store items that are tradable
+ */
 public class TradableItemManager extends Manager<TradableItem> implements Serializable {
-
+    /**
+     * For storing the file path of the .ser file
+     *
+     * @param filePath must take in .ser file
+     * @throws IOException if creating a file causes issues
+     */
     public TradableItemManager(String filePath) throws IOException {
         super(filePath);
     }
 
     /**
      * Creates a new TradableItem.
-     * @param name of the TradableItem to be added
-     * @param description of the TradableItem to be added
+     *
+     * @param name        name of the TradableItem to be added
+     * @param description description of the TradableItem to be added
      * @return id of the item added
      */
-    public String addItem(String name, String description){
+    public String addItem(String name, String description) {
         TradableItem item = new TradableItem(name, description);
         update(item);
         return item.getId();
     }
 
     /**
-     * Removes an item from storage
+     * Removes a TradableItem from storage
+     *
      * @param id the item's id that is being removed
-     * @return the item that got deleted
+     * @return the TradableItem that got deleted
      * @throws EntryNotFoundException if the id doesn't refer to anything
      */
     public TradableItem deleteItem(String id) throws EntryNotFoundException {
--- phase1/src/trades/TradeManager.java
+++ phase1/src/trades/TradeManager.java
@@ -1,12 +1,57 @@
 package trades;
 
+import exceptions.EntryNotFoundException;
 import main.Manager;
-
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Date;
 
+/**
+ * Used to manage and store all trades
+ */
 public class TradeManager extends Manager<Trade> implements Serializable {
+    /**
+     * For storing the file path of the .ser file
+     *
+     * @param filePath must take in .ser file
+     * @throws IOException if creating a file causes issues
+     */
     public TradeManager(String filePath) throws IOException {
         super(filePath);
     }
+
+    /**
+     * Creates a new Trade
+     *
+     * @param firstUserId       the user id of the person initializing the trade
+     * @param secondUserId      the user id of the person the trade is being sent to
+     * @param meetingTime       when the meeting takes place
+     * @param secondMeetingTime when the second meeting takes place
+     * @param meetingLocation   where the meeting takes place
+     * @param firstUserOffer    the item id that the user who initialized the trade is willing to offer
+     * @param secondUserOffer   the item id that the user who got sent the trade is willing to offer
+     * @param allowedEdits      number of edits allowed before the trade is cancelled
+     * @return the id of the new Trade object
+     */
+    public String addTrade(String firstUserId, String secondUserId,
+                           Date meetingTime, Date secondMeetingTime,
+                           String meetingLocation, String firstUserOffer, String secondUserOffer, int allowedEdits) {
+        Trade trade = new Trade(firstUserId, secondUserId,
+                meetingTime, secondMeetingTime,
+                meetingLocation, firstUserOffer, secondUserOffer, allowedEdits);
+        update(trade);
+        return trade.getId();
+    }
+
+    /**
+     * Removes a Trade from storage
+     *
+     * @param id the item's id that is being removed
+     * @return the Trade that got deleted
+     * @throws EntryNotFoundException if the id doesn't refer to anything
+     */
+    public Trade deleteTrade(String id) throws EntryNotFoundException {
+        return super.delete(id);
+    }
+
 }
