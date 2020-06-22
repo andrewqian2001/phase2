a387eaa1c3cb22dd4e523fcffd198474cd55e0df
William Lee <52926429+SSonagi@users.noreply.github.com>
2020-06-22 17:11:02 -0400

    Create constructor Add registerUser and login methods

--- phase1/src/main/TradeSystem.java
+++ phase1/src/main/TradeSystem.java
@@ -1,5 +1,8 @@
 package main;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+import exceptions.UserAlreadyExistsException;
+import exceptions.UserNotFoundException;
 import tradableitems.TradableItem;
 import tradableitems.TradableItemManager;
 import trades.TradeManager;
@@ -11,36 +14,38 @@ import java.io.*;
 import java.util.Map;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TradeSystem implements Serializable {
 
     private UserManager userManager;
+    private UserManager adminUserManager;
     private TradeManager tradeManager;
     private TradableItemManager tradableItemManager;
     private static final Logger LOGGER = Logger.getLogger(Manager.class.getName());
     private static final Handler CONSOLE_HANDLER = new ConsoleHandler();
 
-    private String username;
-    private String password;
     private boolean isAdmin;
 
-    public TradeSystem(String filepath, String username, String password) throws IOException {
+    public TradeSystem(String filepath, String adminFilepath) throws IOException {
+        this.adminUserManager = new UserManager(adminFilepath);
         this.userManager = new UserManager(filepath);
-        this.username = username;
-        this.password = password;
-
-        if(checkAdmin());
     }
 
-    public boolean register() {
-        return userManager.registerUser(username, password, false);
+    public User register(String username, String password) throws UserAlreadyExistsException, UserNotFoundException {
+        if(checkOriginalAdmin(username, password)) {
+            userManager.registerUser(username, password, true);
+        } else {
+            userManager.registerUser(username, password, false);
+        }
+        return login(username, password);
     }
 
+    public User login(String username, String password) throws UserNotFoundException {
+        return userManager.login(username, password);
+    }
 
-
-    private boolean checkAdmin() {
-        return false;
+    private boolean checkOriginalAdmin(String username, String password) {
+        return(adminUserManager.login(username, password).hasPermission());
     }
 }
