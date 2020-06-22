85255e5c7ef0c7d5f06341ba163d24a9e9987e11
William Lee <52926429+SSonagi@users.noreply.github.com>
2020-06-22 17:14:39 -0400

    Added function definitions for register and login

--- phase1/src/users/UserManager.java
+++ phase1/src/users/UserManager.java
@@ -10,4 +10,16 @@ public class UserManager extends Manager<User> implements Serializable {
     public UserManager(String filePath) throws IOException {
         super(filePath);
     }
+
+    //Takes in username and password, checks if the username exists
+    //  if it exists already, return false
+    //  else, create new User,
+    public boolean registerUser(String username, String password, boolean isAdmin) {
+        return false;
+    }
+
+    //Takes in username and password, return user, if user doesn't exist, throw UserNotFoundException
+    public User login(String username, String password) {
+        return null;
+    }
 }
