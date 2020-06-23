97b02198085b55844c6962a8054a468d043a2418
Nilay0921 <61914014+Nilay0921@users.noreply.github.com>
2020-06-23 16:04:04 -0400

    Permissible and Permission updated.

--- phase1/src/users/Admin.java
+++ phase1/src/users/Admin.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class Admin extends User implements Serializable {
 
+    public Admin (String username, String password) {
+        super(username, password);
+    }
+
     /**
      * @return if this user has permission
      */
