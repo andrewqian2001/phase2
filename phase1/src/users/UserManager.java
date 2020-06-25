bb04bcbdf7470f1059cbd1a8ce99af11eb82910e
truanov1 <ilancito7@gmail.com>
2020-06-25 19:57:12 -0400

    Added helper function (findUser) and find method in UserManager. Also simplified code.

--- phase1/src/users/UserManager.java
+++ phase1/src/users/UserManager.java
@@ -33,29 +33,27 @@ public class UserManager extends Manager<User> implements Serializable {
      * @param username the new user's username
      * @param password the new user's password
      * @param userType the specific type of User to be added
-     * @return true if the user was added, false otherwise
+     * @return the new user that was added
      * @throws FileNotFoundException if the specified file path was not found
      * @throws ClassNotFoundException if there is a class that is not defined
      * @throws UserAlreadyExistsException if a user with the same username exists
      */
     public User registerUser(String username, String password, String userType) throws FileNotFoundException, ClassNotFoundException, UserAlreadyExistsException {
-        LinkedList<User> users = getItems();
-        for (User user : users){
-            if (user.getUsername().equals(username)){
-                throw new UserAlreadyExistsException("User already exists.");
+        try{
+            find(username);
+            throw new UserAlreadyExistsException("A user with this username already exists");
+        }
+        catch (UserNotFoundException e){
+            User newUser;
+            switch (userType){
+                case "Admin":
+                    newUser = new Admin(username, password);
+                    break;
+                default:
+                    newUser = new Trader(username, password);
             }
+            return update(newUser);
         }
-        User newUser;
-        switch (userType){
-            case "Admin":
-                newUser = new Admin(username, password);
-                break;
-            default:
-                newUser = new Trader(username, password);
-        }
-        users.add(newUser);
-        save(users);
-        return newUser;
     }
 
 
@@ -70,9 +68,37 @@ public class UserManager extends Manager<User> implements Serializable {
      * @throws ClassNotFoundException if there is a class that is not defined
      */
     public User login(String username, String password) throws UserNotFoundException, FileNotFoundException, ClassNotFoundException {
+        return findUser(username, password, false);
+    }
+
+    /**
+     * Returns a user in the system with the given username.
+     * Throws an error if the user is not found.
+     * @param username the username of the user
+     * @return the user if the user was found.
+     * @throws UserNotFoundException if the user was not found
+     * @throws FileNotFoundException if the specified file path could not be found
+     * @throws ClassNotFoundException if there is a class that is not defined
+     */
+    public User find(String username) throws UserNotFoundException, FileNotFoundException, ClassNotFoundException {
+        return findUser(username, "", true);
+    }
+
+    /**
+     * Helper function to return a user in the system with a specific username (and password if desired).
+     * Throws an error if the user is not found.
+     * @param username the username of the user
+     * @param password the password of the user
+     * @param ignorePass whether to consider the password when finding a user
+     * @return the user if the user was found.
+     * @throws UserNotFoundException if the user was not found
+     * @throws FileNotFoundException if the specified file path could not be found
+     * @throws ClassNotFoundException if there is a class that is not defined
+     */
+    private User findUser(String username, String password, boolean ignorePass) throws UserNotFoundException, FileNotFoundException, ClassNotFoundException{
         LinkedList<User> users = getItems();
         for (User user : users){
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)){
+            if (user.getUsername().equals(username) && (user.getPassword().equals(password) || ignorePass)){
                 return user;
             }
         }
