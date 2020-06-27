0fbaaaa5a72c7aa5277e3fddf6b9d3aa20ae0767
Nilay0921 <61914014+Nilay0921@users.noreply.github.com>
2020-06-27 13:25:18 -0400

    Added a UserNotFoundException

--- phase1/src/main/TextInterface.java
+++ phase1/src/main/TextInterface.java
@@ -9,6 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import exceptions.UserAlreadyExistsException;
+import exceptions.UserNotFoundException;
 import users.Permission;
 import users.User;
 
