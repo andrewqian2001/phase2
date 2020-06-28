5dc9578349582f5c13d8a571ce20caeb9b462f8c
Navinn Ravindaran <home@Navinns-MacBook-Pro.local>
2020-06-28 18:25:26 -0400

    added getRecentTradeItems in TradeSystem

--- phase1/src/main/TradeSystem.java
+++ phase1/src/main/TradeSystem.java
@@ -23,6 +23,7 @@ public class TradeSystem implements Serializable {
 
     /**
      * Constructor for TradeSystem, initializes managers
+     * 
      * @throws IOException
      */
     public TradeSystem() throws IOException {
@@ -34,6 +35,7 @@ public class TradeSystem implements Serializable {
 
     /**
      * Getter method for userID
+     * 
      * @return the id of the loggedInUser
      */
     public String getLoggedInUserId() {
@@ -42,6 +44,7 @@ public class TradeSystem implements Serializable {
 
     /**
      * Registers a new trader into the system
+     * 
      * @param username username of new trader
      * @param password password for new trader
      * @return id of the newly registered trader
@@ -56,6 +59,7 @@ public class TradeSystem implements Serializable {
 
     /**
      * Registers a new Admin into the system
+     * 
      * @param username username of new admin
      * @param password password for new admin
      * @throws IOException
@@ -63,11 +67,12 @@ public class TradeSystem implements Serializable {
      */
     public void registerAdmin(String username, String password) throws IOException, UserAlreadyExistsException {
         userManager = new AdminManager(USERS_FILE_PATH);
-        ((AdminManager)userManager).registerUser(username, password);
+        ((AdminManager) userManager).registerUser(username, password);
     }
 
     /**
      * Logs-in a current user into the system
+     * 
      * @param username username of existing user
      * @param password password of existing user
      * @return id of newly logged in user
@@ -87,8 +92,8 @@ public class TradeSystem implements Serializable {
     }
 
     /**
-     * Check if a User, given their ID, is frozen 
-     * NOTE: This method is not for Admins since their accounts cannot be frozen
+     * Check if a User, given their ID, is frozen NOTE: This method is not for
+     * Admins since their accounts cannot be frozen
      * 
      * @param userID id of the user
      * @return true if the user is frozen, false else
@@ -110,20 +115,24 @@ public class TradeSystem implements Serializable {
     }
 
     /**
-     * Freezes/Unfreezes a Trader given their username
-     * Requirement: Only an Admin Account can preform this action
-     * @param username the username of the Trader that needs to be (un-)frozen
-     * @param freezeStatus if true, method will freeze the Trader, else it will unFreeze
+     * Freezes/Unfreezes a Trader given their username Requirement: Only an Admin
+     * Account can preform this action
+     * 
+     * @param username     the username of the Trader that needs to be (un-)frozen
+     * @param freezeStatus if true, method will freeze the Trader, else it will
+     *                     unFreeze
      * @throws EntryNotFoundException
      * @throws AuthorizationException
      */
-    public void freezeUser(String username, boolean freezeStatus) throws EntryNotFoundException, AuthorizationException {
+    public void freezeUser(String username, boolean freezeStatus)
+            throws EntryNotFoundException, AuthorizationException {
         String userId = getIdFromUsername(username);
         userManager.freezeUser(loggedInUserId, userId, freezeStatus);
     }
 
     /**
      * Gets the name of the tradable item given its id
+     * 
      * @param tradableItemId id of the tradable item
      * @return name of the tradable item
      * @throws EntryNotFoundException
@@ -134,6 +143,7 @@ public class TradeSystem implements Serializable {
 
     /**
      * Gets the description of the tradable item given its id
+     * 
      * @param tradableItemId id of the tradable item
      * @return description of the tradable item
      * @throws EntryNotFoundException
@@ -143,8 +153,9 @@ public class TradeSystem implements Serializable {
     }
 
     /**
-     * Gets the username of a User given their ID
-     * NOTE: This will most likely be deleted before rollout since theres no use for this
+     * Gets the username of a User given their ID NOTE: This will most likely be
+     * deleted before rollout since theres no use for this
+     * 
      * @param userId id of the User
      * @return username of the User
      * @throws EntryNotFoundException
@@ -153,14 +164,14 @@ public class TradeSystem implements Serializable {
         return userManager.getUsername(userId);
     }
 
-
     /**
      * Gets the id of a User given their username
+     * 
      * @param username username of the User
      * @return id of the User
      * @throws EntryNotFoundException
      */
-    public String getIdFromUsername(String username) throws EntryNotFoundException{
+    public String getIdFromUsername(String username) throws EntryNotFoundException {
         return userManager.getUserId(username);
     }
 
@@ -172,9 +183,9 @@ public class TradeSystem implements Serializable {
      * @throws EntryNotFoundException
      */
     public ArrayList<String> getWishlist(String userID) throws EntryNotFoundException {
-        return ((TraderManager)userManager).getWishlist(userID);
+        return ((TraderManager) userManager).getWishlist(userID);
     }
-    
+
     /**
      * Gets a list of a given user's Available Items
      * 
@@ -183,9 +194,9 @@ public class TradeSystem implements Serializable {
      * @throws EntryNotFoundException
      */
     public ArrayList<String> getAvailableItems(String userID) throws EntryNotFoundException {
-        return ((TraderManager)userManager).getAvailableItems(userID);
+        return ((TraderManager) userManager).getAvailableItems(userID);
     }
-    
+
     /**
      * Gets a list of a given user's Accepted Trades
      * 
@@ -194,21 +205,23 @@ public class TradeSystem implements Serializable {
      * @throws EntryNotFoundException
      */
     public ArrayList<String> getAcceptedTrades(String userID) throws EntryNotFoundException {
-        return ((TraderManager)userManager).getAcceptedTrades(userID);
+        return ((TraderManager) userManager).getAcceptedTrades(userID);
     }
-    
+
     /**
      * Gets a list of a given user's Requested Trades
+     * 
      * @param userID the id of the user
      * @return a list of id's of Requested Trades
      * @throws EntryNotFoundException
      */
     public ArrayList<String> getRequestedTrades(String userID) throws EntryNotFoundException {
-        return ((TraderManager)userManager).getRequestedTrades(userID);
+        return ((TraderManager) userManager).getRequestedTrades(userID);
     }
 
     /**
      * Gets a list of all traders in the database
+     * 
      * @return A list of all traders in the database
      * @throws EntryNotFoundException
      */
@@ -218,56 +231,35 @@ public class TradeSystem implements Serializable {
 
     /**
      * Requests that the item be added to the user's iventory
+     * 
      * @param userID user ID
-     * @param item item object
+     * @param item   item object
      * @throws EntryNotFoundException
      */
     public void requestItem(String userID, TradableItem item) throws EntryNotFoundException {
-        ((TraderManager)userManager).addRequestItem(userID, item.getId());
+        ((TraderManager) userManager).addRequestItem(userID, item.getId());
     }
 
     /**
      * get all items in all user's inventories
+     * 
      * @return hash map of the items
      * @throws EntryNotFoundException
      */
     public HashMap<String, ArrayList<String>> getAllAvailableItems() throws EntryNotFoundException {
-        return ((TraderManager)userManager).getAllItemsInInventories();
+        return ((TraderManager) userManager).getAllItemsInInventories();
     }
 
     /**
      * add item to wish list
+     * 
      * @param userID user ID
-     * @param item item object
+     * @param item   item object
      * @throws EntryNotFoundException
      */
     public void addToWishList(String userID, TradableItem item) throws EntryNotFoundException {
-        ((TraderManager)userManager).addToWishList(userID, item.getId());
+        ((TraderManager) userManager).addToWishList(userID, item.getId());
     }
-
-    /*
-    public void addItem(String ID, TradableItem item) {
-        Trader user = null;
-        try {
-            user = (Trader) userManager.populate(ID);
-        } catch (EntryNotFoundException e) {
-            // Not sure what im supposed to do when I get an exception
-        } finally {
-            if (user != null && user.hasPermission(Permission.CONFIRM_ADDED_ITEM)) {
-                ArrayList<String> inventory = user.getAvailableItems();
-                ArrayList<String> reqList = user.getRequestedItems();
-
-                if (reqList.contains(item.getId())) {
-                    inventory.add(item.getId());
-                    user.setAvailableItems(inventory);
-                    reqList.remove(item.getId());
-                    user.setRequestedItems(reqList);
-                    userManager.update(user);
-                }
-            }
-        }
-
-    }*/
 
     /**
      * @param userId the user that wants to be unfrozen
@@ -280,16 +272,17 @@ public class TradeSystem implements Serializable {
 
     /**
      * return the 3 most traded with Traders
+     * 
      * @param userID user Id
      * @return a String array of the user Ids of the 3 most traded with Traders
      * @throws EntryNotFoundException
      */
     public String[] getFrequentTraders(String userID) throws EntryNotFoundException {
         String[] frequentTraders = new String[3];
-        ArrayList<String> acceptedTrades = ((TraderManager)userManager).getAcceptedTrades(userID); // Trade IDs
-        Set<String> distinct =  new HashSet<>(acceptedTrades);
+        ArrayList<String> acceptedTrades = ((TraderManager) userManager).getAcceptedTrades(userID); // Trade IDs
+        Set<String> distinct = new HashSet<>(acceptedTrades);
         int highest = 0;
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             for (String s : distinct) {
                 if (Collections.frequency(acceptedTrades, s) > highest) {
                     frequentTraders[i] = s;
@@ -300,7 +293,20 @@ public class TradeSystem implements Serializable {
         return frequentTraders;
     }
 
-    public ArrayList<String> getAllUnfreezeRequests() throws EntryNotFoundException{
-        return ((AdminManager)userManager).getAllUnFreezeRequests();
+    public Set<String> getRecentTradeItems(String userId) throws EntryNotFoundException {
+        ArrayList<String> acceptedTrades = ((TraderManager) userManager).getAcceptedTrades(userId);
+        Set<String> recentTradeItemNames = new HashSet<>();
+        for (String tradeID : acceptedTrades) {
+            // TO-DO: ADD IN TRADERMANAGER
+            String[] tradeableItemIDs = ((TraderManager) userManager).getItemsFromTrade(tradeID);
+            recentTradeItemNames.add(getTradableItemName(tradeableItemIDs[0]));
+            if (!tradeableItemIDs[1].equals(""))
+                recentTradeItemNames.add(getTradableItemName(tradeableItemIDs[1]));
+        }
+        return recentTradeItemNames;
+    }
+
+    public ArrayList<String> getAllUnfreezeRequests() throws EntryNotFoundException {
+        return ((AdminManager) userManager).getAllUnFreezeRequests();
     }
 }
