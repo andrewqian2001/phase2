a582f8e251656899a51e07ef42b9d5d40c02b21f
James <smurf.lee@mail.utoronto.ca>
2020-07-11 09:32:19 -0400

    register admin in admin account

--- phase1/src/main/TradeSystem/Accounts/AdminAccount.java
+++ phase1/src/main/TradeSystem/Accounts/AdminAccount.java
@@ -3,13 +3,15 @@ package main.TradeSystem.Accounts;
 import java.io.IOException;
 
 import exceptions.AuthorizationException;
-import exceptions.EntryNotFoundException;
+import exceptions.UserAlreadyExistsException;
 import exceptions.UserNotFoundException;
 import main.TradeSystem.Managers.AdminManager;
+import main.TradeSystem.Managers.LoginManager;
 
 public class AdminAccount implements Account {
 
     private final AdminManager adminManager;
+    private final LoginManager loginManager;
 
     /**
      * For accessing actions that an admin can do
@@ -20,11 +22,23 @@ public class AdminAccount implements Account {
      */
     public AdminAccount(String adminId) throws IOException, UserNotFoundException, AuthorizationException {
         adminManager = new AdminManager(adminId);
+        loginManager = new LoginManager();
     }
 
-    // Needs registerAdmin method here
+    /**
+     * Registers a new admin
+     * @param username username of new admin
+     * @param password password of new admin
+     * @return the id of the new admin
+     * @throws UserAlreadyExistsException if the username already exists
+     */
+    public String registerAdmin (String username, String password) throws UserAlreadyExistsException {
+        return loginManager.registerUser(username, password, UserTypes.ADMIN);
+    }
 
-
+    /**
+     * @return type admin
+     */
     @Override
     public UserTypes getAccountType() {
         return UserTypes.ADMIN;
