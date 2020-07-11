81a01b2d1bec5e7f4d3b9888d5c3e86ec4b09546
James <smurf.lee@mail.utoronto.ca>
2020-07-11 06:23:04 -0400

    javadoc and syntax fix in loginmanager

--- phase1/src/main/TradeSystem/Managers/LoginManager.java
+++ phase1/src/main/TradeSystem/Managers/LoginManager.java
@@ -11,6 +11,9 @@ import main.DatabaseFilePaths;
 import java.io.IOException;
 import java.util.LinkedList;
 
+/**
+ * Used for logging in and registering
+ */
 public class LoginManager {
     private final Database<User> userDatabase;
     private int defaultTradeLimit = 10;
@@ -45,7 +48,7 @@ public class LoginManager {
      */
     public String registerUser(String username, String password, UserTypes type) throws UserAlreadyExistsException {
         if (!isUsernameUnique(username))
-            throw new UserAlreadyExistsException("A user with the username " + username + " exists already.");
+            throw new UserAlreadyExistsException();
         switch (type) {
             case ADMIN:
                 return userDatabase.update(new Admin(username, password)).getId();
@@ -99,7 +102,8 @@ public class LoginManager {
 
     /**
      * For the trader user type, this sets the initial incomplete trade limit when making a new account.
-     * @param defaultIncompleteTradeLim
+     *
+     * @param defaultIncompleteTradeLim initial incomplete trade limit of a trader user
      */
     public void setDefaultIncompleteTradeLim(int defaultIncompleteTradeLim) {
         this.defaultIncompleteTradeLim = defaultIncompleteTradeLim;
@@ -108,7 +112,8 @@ public class LoginManager {
     /**
      * For the trader user type, this sets the initial minimum amount that (items lent - items borrowed) should be
      * before the trader is allowed to borrow.
-     * @param defaultMinimumAmountNeededToBorrow
+     *
+     * @param defaultMinimumAmountNeededToBorrow the number of items that need to be lent out before borrowing is allowed
      */
     public void setDefaultMinimumAmountNeededToBorrow(int defaultMinimumAmountNeededToBorrow) {
         this.defaultMinimumAmountNeededToBorrow = defaultMinimumAmountNeededToBorrow;
