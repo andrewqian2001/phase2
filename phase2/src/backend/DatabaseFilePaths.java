ea0b98a3cfdab40dee79f07733e852d4e939e20a
James <smurf.lee@mail.utoronto.ca>
2020-07-15 04:42:38 -0400

    new packages

--- phase2/src/backend/DatabaseFilePaths.java
+++ phase2/src/backend/DatabaseFilePaths.java
@@ -7,19 +7,19 @@ public enum DatabaseFilePaths {
     /**
      * For the file path of all users
      */
-    USER("./phase1/src/Database/users/users.ser"),
+    USER("./phase1/src/backend/database/users/users.ser"),
     /**
      * file path of all trades
      */
-    TRADE("./phase1/src/Database/trades/trades.ser"),
+    TRADE("./phase1/src/backend/database/trades/trades.ser"),
     /**
      * file path for all items that are traded
      */
-    TRADABLE_ITEM("./phase1/src/Database/tradableitems/tradableitems.ser"),
+    TRADABLE_ITEM("./phase1/src/backend/database/tradableitems.ser"),
     /**
      * file path for trader config file
      */
-    TRADER_CONFIG("./phase1/src/main/trader.properties");
+    TRADER_CONFIG("./phase1/src/backend/tradesystem/managers/trader.properties");
     private final String FILE_PATH;
 
     DatabaseFilePaths(String filePath) {
--- phase2/src/frontend/Main.java
+++ phase2/src/frontend/Main.java
@@ -7,7 +7,5 @@ import java.io.*;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        TextInterface tInterface = new TextInterface();
-        tInterface.run();
     }
 }
