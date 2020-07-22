462139f0ba84bec69dfaac9cbb4b80a31e0e3fdc
William Lee <52926429+SSonagi@users.noreply.github.com>
2020-07-22 15:57:27 -0400

    SearchPanel is used for both AdminPanel ad TraderPanel now

--- phase2/src/frontend/panels/SearchPanel.java
+++ phase2/src/frontend/panels/SearchPanel.java
@@ -1,13 +1,14 @@
-package frontend.panels.trader_subpanels;
+package frontend.panels;
 
 import javax.swing.JPanel;
 
 import java.awt.*;
 
 import backend.models.users.Trader;
+import backend.models.users.User;
 
 public class SearchPanel extends JPanel {
-    public SearchPanel(Trader trader, Font regular, Font bold, Font italic, Font boldItalic) {
+    public SearchPanel(User user, Font regular, Font bold, Font italic, Font boldItalic) {
         this.setSize(1000, 900);
         this.setBackground(Color.CYAN);
     }
