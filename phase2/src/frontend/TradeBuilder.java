0acb22d118aaf4c5d0a5430359f02a9213e7740c
truanov1 <ilancito7@gmail.com>
2020-07-24 16:10:45 -0400

    Added a TradeBuilder to be used in frontend.

---
+++ phase2/src/frontend/TradeBuilder.java
@@ -0,0 +1,59 @@
+package frontend;
+
+import backend.models.Trade;
+
+import java.util.Date;
+
+public class TradeBuilder {
+
+    private String firstUserId;
+    private String secondUserId;
+    private int allowedEditsPerUser;
+    private String location;
+    private Date meeting1;
+    private Date meeting2;
+    private String firstUserOffer;
+    private String secondUserOffer;
+    private String message;
+
+    public void setFirstUser(String firstUserId){
+        this.firstUserId = firstUserId;
+    }
+
+    public void setSecondUser(String secondUserId){
+        this.secondUserId = secondUserId;
+    }
+
+    public void setAllowedEditsPerUser(int allowedEditsPerUser){
+        this.allowedEditsPerUser = allowedEditsPerUser;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public void setMeeting1(Date meeting1){
+        this.meeting1 = meeting1;
+    }
+
+    public void setMeeting2(Date meeting2){
+        this.meeting2 = meeting2;
+    }
+
+    public void setFirstUserOffer(String firstUserOffer){
+        this.firstUserOffer = firstUserOffer;
+    }
+
+    public void setSecondUserOffer(String secondUserOffer){
+        this.secondUserOffer = secondUserOffer;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public Trade createTrade(){
+        return new Trade(firstUserId, secondUserId, meeting1, meeting2, location, firstUserOffer, secondUserOffer, allowedEditsPerUser, message);
+    }
+
+}
