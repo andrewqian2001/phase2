bfba051b53bb774a5506b9b967303520baaf8409
James <smurf.lee@mail.utoronto.ca>
2020-07-27 16:32:08 -0400

    temporary setup for testing purposes has more stuff now

--- phase2/src/frontend/TemporarySetup.java
+++ phase2/src/frontend/TemporarySetup.java
@@ -18,52 +18,43 @@ import java.util.Properties;
 
 public class TemporarySetup {
 
-    private Trader trader1;
-    private Trader trader2;
-    private Admin admin;
+    private Trader[] traders;
+    private Admin[] admins;
     private LoginManager loginManager;
     private TraderManager traderManager;
     private HandleItemRequestsManager handleRequestsManager;
-    public TemporarySetup(){
+
+    public TemporarySetup() {
+        traders = new Trader[10];
+        admins = new Admin[5];
         refreshFiles();
         try {
             loginManager = new LoginManager();
             traderManager = new TraderManager();
             handleRequestsManager = new HandleItemRequestsManager();
-            trader1 = (Trader)loginManager.registerUser("user", "passssssssS11", UserTypes.TRADER);
-            trader2 = (Trader)loginManager.registerUser("user1", "passssssssS11", UserTypes.TRADER);
-            admin = (Admin) loginManager.registerUser("admin", "passssssssS11", UserTypes.ADMIN);
-            traderManager.addRequestItem(trader1.getId(), "apple", "sweet");
-            traderManager.addRequestItem(trader1.getId(), "apple1", "sweet1");
-            traderManager.addRequestItem(trader1.getId(), "apple2", "sweet2");
-            traderManager.addRequestItem(trader2.getId(), "pear1", "disgusting");
-            traderManager.addRequestItem(trader2.getId(), "pear2", "disgusting2");
-            traderManager.addRequestItem(trader2.getId(), "pear3", "disgusting3");
-
-
-            update();
-            handleRequestsManager.processItemRequest(trader1.getId(), trader1.getRequestedItems().get(0), true);
-            handleRequestsManager.processItemRequest(trader1.getId(), trader1.getRequestedItems().get(1), true);
-            handleRequestsManager.processItemRequest(trader1.getId(), trader1.getRequestedItems().get(2), true);
-            handleRequestsManager.processItemRequest(trader2.getId(), trader2.getRequestedItems().get(0), true);
-            handleRequestsManager.processItemRequest(trader2.getId(), trader2.getRequestedItems().get(1), true);
-            handleRequestsManager.processItemRequest(trader2.getId(), trader2.getRequestedItems().get(2), true);
-
-            update();
-            traderManager.addToWishList(trader1.getId(), trader2.getAvailableItems().get(0));
-            traderManager.addToWishList(trader1.getId(), trader2.getAvailableItems().get(1));
-            traderManager.addToWishList(trader2.getId(), trader1.getAvailableItems().get(0));
-            traderManager.addToWishList(trader2.getId(), trader1.getAvailableItems().get(1));
-
-
-
-        }catch (IOException | UserAlreadyExistsException | BadPasswordException | UserNotFoundException | AuthorizationException | TradableItemNotFoundException e){
+            for (int i = 0; i < traders.length; i++){
+                traders[i] = (Trader) loginManager.registerUser("trader" + i, "passssssssS11", UserTypes.TRADER);
+                traders[i] = traderManager.addRequestItem(traders[i].getId(), "apple" + i, "sweet" + i);
+                traders[i] = traderManager.addRequestItem(traders[i].getId(), "banananana" + i, "disgusting" + i);
+                traders[i] = traderManager.addRequestItem(traders[i].getId(), "kiwi" + i, "from oceania" + i);
+                traders[i] = handleRequestsManager.processItemRequest(traders[i].getId(), traders[i].getRequestedItems().get(0), true);
+                traders[i] = handleRequestsManager.processItemRequest(traders[i].getId(), traders[i].getRequestedItems().get(0), true);
+                traders[i] = handleRequestsManager.processItemRequest(traders[i].getId(), traders[i].getRequestedItems().get(0), true);
+                traders[i] = traderManager.addRequestItem(traders[i].getId(), "requested" + i, "requested desc" + i);
+                traders[i] = traderManager.addRequestItem(traders[i].getId(), "another requested" + i, "bad desc requested" + i);
+            }
+            for (int i = 0; i < admins.length; i++){
+                admins[i] = (Admin) loginManager.registerUser("admin" + i, "PasswordPassword1!", UserTypes.ADMIN);
+            }
+            for (int i = 0; i < traders.length; i++)
+                traderManager.addToWishList(traders[i].getId(), traders[i - 1 == -1 ? traders.length - 1 : i].getAvailableItems().get(0));
+        } catch (IOException | UserAlreadyExistsException | BadPasswordException | UserNotFoundException | AuthorizationException | TradableItemNotFoundException e) {
 
         }
     }
 
 
-    private void refreshFiles(){
+    private void refreshFiles() {
         String[] paths = {DatabaseFilePaths.TRADE.getFilePath(), DatabaseFilePaths.TRADABLE_ITEM.getFilePath(),
                 DatabaseFilePaths.USER.getFilePath()};
         for (String path : paths) {
@@ -79,11 +70,13 @@ public class TemporarySetup {
         setProperty(TraderProperties.MINIMUM_AMOUNT_NEEDED_TO_BORROW, 1);
         setProperty(TraderProperties.TRADE_LIMIT, 10);
     }
-    private void update(){
+
+    private void update() {
         try {
-            trader1 = traderManager.getTrader(trader1.getId());
-            trader2 = traderManager.getTrader(trader2.getId());
-            admin = (Admin) traderManager.getUser(admin.getId());
+            for (int i = 0; i < traders.length; i++)
+                traders[i] = traderManager.getTrader(traders[i].getId());
+            for (int i = 0; i < admins.length; i++)
+                admins[i] = (Admin) traderManager.getUser(admins[i].getId());
 
         } catch (UserNotFoundException | AuthorizationException e) {
             e.printStackTrace();
@@ -92,36 +85,12 @@ public class TemporarySetup {
     }
 
     /**
-     * Gets the current value of the specified trader property
-     * @param propertyType the type of property
-     * @return the value of the specified trader property
-     */
-    private int getProperty(TraderProperties propertyType){
-        try {
-            // get the file
-            File propertyFile = new File(DatabaseFilePaths.TRADER_CONFIG.getFilePath());
-            // initialize the reader of this file
-            FileReader reader = new FileReader(propertyFile);
-            // initialize properties object
-            Properties properties = new Properties();
-            // associate properties object with this file.
-            properties.load(reader);
-            // we're not going to use reader anymore, so close it
-            reader.close();
-            // return the integer value of that property
-            return Integer.parseInt(properties.getProperty(propertyType.getProperty()));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return -1;
-    }
-
-    /**
      * Sets the value of a property.
-     * @param propertyName the property to change
+     *
+     * @param propertyName  the property to change
      * @param propertyValue the new value of that property
      */
-    private void setProperty(TraderProperties propertyName, int propertyValue){
+    private void setProperty(TraderProperties propertyName, int propertyValue) {
         try {
             // get the file
             File propertyFile = new File(DatabaseFilePaths.TRADER_CONFIG.getFilePath());
