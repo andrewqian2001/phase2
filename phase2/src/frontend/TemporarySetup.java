9d3b8cb1c3ddf01a8c6dba69184928c3a9e6a728
James <smurf.lee@mail.utoronto.ca>
2020-07-27 19:01:02 -0400

    trades in temporarysetup

--- phase2/src/frontend/TemporarySetup.java
+++ phase2/src/frontend/TemporarySetup.java
@@ -2,6 +2,7 @@ package frontend;
 
 import backend.DatabaseFilePaths;
 import backend.exceptions.*;
+import backend.models.Trade;
 import backend.models.users.Admin;
 import backend.models.users.Trader;
 import backend.tradesystem.TraderProperties;
@@ -10,6 +11,7 @@ import backend.tradesystem.managers.*;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -30,7 +32,7 @@ public class TemporarySetup {
             HandleItemRequestsManager handleRequestsManager = new HandleItemRequestsManager();
             HandleFrozenManager handleFrozenManager = new HandleFrozenManager();
             MessageManager messageManager = new MessageManager();
-
+            TradingManager tradingManager = new TradingManager();
             // Each trader has some items that are confirmed and not confirmed
             // Username is trader{index here from 0 to 9 inclusive}
             // Password is 'userPassword1'
@@ -46,9 +48,37 @@ public class TemporarySetup {
                 traders[i] = traderManager.addRequestItem(traders[i].getId(), "another requested" + i, "bad desc requested" + i);
                 traders[i] = traderManager.setCity(traders[i].getId(), "Toronto");
             }
+            Date goodDate = new Date(System.currentTimeMillis() + 99999999);
+            Date goodDate2 = new Date(System.currentTimeMillis() + 999999999);
+            // Trades
+            for (int i = 0; i < traders.length - 1; i++){
+                try {
+                    Trade acceptThis = tradingManager.requestTrade(new Trade(traders[i].getId(), traders[i + 1].getId(), goodDate, goodDate2,
+                            "123 bay street", traders[i].getAvailableItems().get(0), traders[i + 1].getAvailableItems().get(0),
+                            3, "give me your apple " + i)); // This is a temp trade
+                    Trade ongoing = tradingManager.requestTrade(new Trade(traders[i].getId(), traders[i + 1].getId(), goodDate, null,
+                            "123 bay street", traders[i].getAvailableItems().get(1), traders[i + 1].getAvailableItems().get(1),
+                            3, "give me your banana " + i)); // This is a perma trade
+                    Trade requestedOnly = tradingManager.requestTrade(new Trade(traders[i].getId(), traders[i + 1].getId(), goodDate, goodDate2,
+                            "123 bay street", traders[i].getAvailableItems().get(2), "",
+                            3, "I give you my kiwi " + i)); // This is temporary lending
+                    // Only accepts request and doesn't confirm meetings so trade is ongoing
+                    tradingManager.acceptRequest(traders[i+1].getId(), ongoing.getId());
+                    // Confirms four meetings for a temporary trade and accepts request, meaning the trade is complete
+                    tradingManager.acceptRequest(traders[i+1].getId(), acceptThis.getId());
+                    tradingManager.confirmMeetingGeneral(traders[i].getId(), acceptThis.getId(), true);
+                    tradingManager.confirmMeetingGeneral(traders[i + 1].getId(), acceptThis.getId(), true);
+                    tradingManager.confirmMeetingGeneral(traders[i].getId(), acceptThis.getId(), true);
+                    tradingManager.confirmMeetingGeneral(traders[i + 1].getId(), acceptThis.getId(), true);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
             // Each trader has a wishlist of one item
             for (int i = 0; i < traders.length; i++)
-                traders[i] = traderManager.addToWishList(traders[i].getId(), traders[i - 1 == -1 ? traders.length - 1 : i].getAvailableItems().get(0));
+                traders[i] = traderManager.addToWishList(traders[i].getId(), traders[i - 1 == -1 ? traders.length - 1 : i - 1].getAvailableItems().get(0));
             // For changing cities
             traders[3] = traderManager.setCity(traders[3].getId(), "new york");
             traders[4] = traderManager.setCity(traders[4].getId(), "new york");
