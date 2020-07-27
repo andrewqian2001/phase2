0b2c87ee8e0411c4def3efb3fa0c9687f32936f4
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-07-26 23:06:55 -0400

    removed itemPanel.java and migrated everything to itemsPanel.java

--- phase2/src/frontend/panels/trader_subpanels/ItemsPanel.java
+++ phase2/src/frontend/panels/trader_subpanels/ItemsPanel.java
@@ -2,10 +2,13 @@ package frontend.panels.trader_subpanels;
 
 import javax.swing.*;
 
+import backend.exceptions.TradableItemNotFoundException;
+import backend.models.TradableItem;
 import backend.models.users.Trader;
 import backend.tradesystem.managers.TradingManager;
 
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ItemsPanel extends JPanel {
@@ -25,7 +28,7 @@ public class ItemsPanel extends JPanel {
 
     private TradingManager tradeManager;
 
-    public ItemsPanel(Trader trader, Font regular, Font bold, Font italic, Font boldItalic) {
+    public ItemsPanel(Trader trader, Font regular, Font bold, Font italic, Font boldItalic) throws IOException {
 
         this.trader = trader;
         this.regular = regular;
@@ -36,7 +39,9 @@ public class ItemsPanel extends JPanel {
         this.setSize(1000, 900);
         this.setBackground(bg);
 
-        inventoryTitleContainer = new JPanel(new GridLayout(1,2));
+        tradeManager = new TradingManager();
+
+        inventoryTitleContainer = new JPanel(new GridLayout(1, 2));
         inventoryTitleContainer.setPreferredSize(new Dimension(1200, 75));
         inventoryTitleContainer.setBackground(bg);
 
@@ -55,8 +60,10 @@ public class ItemsPanel extends JPanel {
         inventoryItemsScrollPane = new JScrollPane();
         inventoryItemsScrollPane.setBorder(null);
         inventoryItemsScrollPane.setPreferredSize(new Dimension(1200, 325));
+        getInventory();
+        inventoryItemsScrollPane.setViewportView(inventoryItemsContainer);
 
-        wishlistTitleContainer = new JPanel(new GridLayout(1,2));
+        wishlistTitleContainer = new JPanel(new GridLayout(1, 2));
         wishlistTitleContainer.setPreferredSize(new Dimension(1200, 75));
         wishlistTitleContainer.setBackground(bg);
 
@@ -75,6 +82,8 @@ public class ItemsPanel extends JPanel {
         wishlistItemsScrollPane = new JScrollPane();
         wishlistItemsScrollPane.setBorder(null);
         wishlistItemsScrollPane.setPreferredSize(new Dimension(1200, 325));
+        getWishlist();
+        wishlistItemsScrollPane.setViewportView(wishlistItemsContainer);
 
         inventoryTitleContainer.add(inventoryTitle);
         inventoryTitleContainer.add(addInventoryItemButton);
@@ -90,10 +99,86 @@ public class ItemsPanel extends JPanel {
 
     private void getInventory() {
         // trader.getAvailableItems();
+        inventoryItemsContainer = new JPanel(new GridLayout(trader.getAvailableItems().size(), 1));
+        inventoryItemsContainer.setBackground(gray);
+        inventoryItemsContainer.setBorder(null);
+
+        for (String itemId : trader.getAvailableItems()) {
+            try {
+                TradableItem item = tradeManager.getTradableItem(itemId);
+                JPanel itemPanel = new JPanel(new GridLayout(1, 5, 10, 0));
+                itemPanel.setPreferredSize(new Dimension(1000, 75));
+                itemPanel.setBackground(gray);
+                itemPanel.setBorder(BorderFactory.createLineBorder(bg));
+
+                JLabel itemName = new JLabel(item.getName());
+                itemName.setFont(regular.deriveFont(20f));
+                itemName.setForeground(Color.BLACK);
+                itemName.setHorizontalAlignment(JLabel.LEFT);
+                itemName.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
+
+                JLabel itemDesc = new JLabel(item.getDesc());
+                itemDesc.setFont(regular.deriveFont(20f));
+                itemDesc.setForeground(Color.BLACK);
+                itemDesc.setHorizontalAlignment(JLabel.LEFT);
+
+                JButton removeitemButton = new JButton("Remove");
+                removeitemButton.setFont(bold.deriveFont(20f));
+                removeitemButton.setForeground(Color.WHITE);
+                removeitemButton.setBackground(red);
+                removeitemButton.setOpaque(true);
+                removeitemButton.setBorder(BorderFactory.createLineBorder(gray, 15));
+
+                itemPanel.add(itemName);
+                itemPanel.add(itemDesc);
+                itemPanel.add(removeitemButton);
+                inventoryItemsContainer.add(itemPanel);
+            } catch (TradableItemNotFoundException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
     
     private void getWishlist() {
         // trader.getWishlist();
+        wishlistItemsContainer = new JPanel(new GridLayout(trader.getWishlist().size(), 1));
+        wishlistItemsContainer.setBackground(gray);
+        wishlistItemsContainer.setBorder(null);
+        
+        for (String itemId : trader.getWishlist()) {
+            try {
+                TradableItem item = tradeManager.getTradableItem(itemId);
+                JPanel itemPanel = new JPanel(new GridLayout(1, 5, 10, 0));
+                itemPanel.setPreferredSize(new Dimension(1000, 75));
+                itemPanel.setBackground(gray);
+                itemPanel.setBorder(BorderFactory.createLineBorder(bg));
 
+                JLabel itemName = new JLabel(item.getName());
+                itemName.setFont(regular.deriveFont(20f));
+                itemName.setForeground(Color.BLACK);
+                itemName.setHorizontalAlignment(JLabel.LEFT);
+                itemName.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
+
+                JLabel itemDesc = new JLabel(item.getDesc());
+                itemDesc.setFont(regular.deriveFont(20f));
+                itemDesc.setForeground(Color.BLACK);
+                itemDesc.setHorizontalAlignment(JLabel.LEFT);
+
+                JButton removeitemButton = new JButton("Remove");
+                removeitemButton.setFont(bold.deriveFont(20f));
+                removeitemButton.setForeground(Color.WHITE);
+                removeitemButton.setBackground(red);
+                removeitemButton.setOpaque(true);
+                removeitemButton.setBorder(BorderFactory.createLineBorder(gray, 15));
+
+                itemPanel.add(itemName);
+                itemPanel.add(itemDesc);
+                itemPanel.add(removeitemButton);
+                wishlistItemsContainer.add(itemPanel);
+
+            } catch (TradableItemNotFoundException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 }
