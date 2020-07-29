b200c5650556209b4b91e29573707007565587c3
William Lee <52926429+SSonagi@users.noreply.github.com>
2020-07-29 17:13:19 -0400

    Fix warnings on JComboBox

--- phase2/src/frontend/panels/trader_subpanels/TradePanel.java
+++ phase2/src/frontend/panels/trader_subpanels/TradePanel.java
@@ -933,13 +933,13 @@ public class TradePanel extends JPanel implements ActionListener {
         meetingInput.setPreferredSize(new Dimension(290, 50));
 
         String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        JComboBox month = new JComboBox(months);
+        JComboBox<String> month = new JComboBox<>(months);
         month.setPreferredSize(new Dimension(100, 50));
         meetingInput.add(month);
 
         String[] days = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
                 "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
-        JComboBox day = new JComboBox(days);
+        JComboBox<String> day = new JComboBox<>(days);
         day.setPreferredSize(new Dimension(60, 50));
         meetingInput.add(day);
 
