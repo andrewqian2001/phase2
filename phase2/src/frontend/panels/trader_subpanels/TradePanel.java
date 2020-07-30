58858689109c4ba0fa8829e61c2c8c0e237fa831
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-07-29 22:13:30 -0400

    fixed bad import

--- phase2/src/frontend/panels/trader_subpanels/TradePanel.java
+++ phase2/src/frontend/panels/trader_subpanels/TradePanel.java
@@ -1,14 +1,31 @@
 package frontend.panels.trader_subpanels;
 
-import javax.swing.*;
-
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import backend.exceptions.AuthorizationException;
 import backend.exceptions.CannotTradeException;
@@ -20,7 +37,7 @@ import backend.models.Trade;
 import backend.models.users.Trader;
 import backend.tradesystem.managers.TradingInfoManager;
 import backend.tradesystem.managers.TradingManager;
-import frontend.TradeBuilder;
+import backend.tradesystem.TradeBuilder;
 
 public class TradePanel extends JPanel implements ActionListener {
 
