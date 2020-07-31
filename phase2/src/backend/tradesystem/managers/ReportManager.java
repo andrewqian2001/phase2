d8341d3b5e757126323072349a6a2e0d2c5168e7
James <smurf.lee@mail.utoronto.ca>
2020-07-31 17:07:18 -0400

    message manager restructuring

--- phase2/src/backend/tradesystem/managers/ReportManager.java
+++ phase2/src/backend/tradesystem/managers/ReportManager.java
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Used for managing notifications and messages
+ * Used for managing reports
  */
-public class MessageManager extends Manager {
+public class ReportManager extends Manager {
 
 
     /**
@@ -86,4 +86,74 @@ public class MessageManager extends Manager {
         User user = getUser(userId);
         return user.getMessages();
     }
+
+    /**
+     * Reporting a user
+     *
+     * @param fromUserId the user that sent the report
+     * @param toUserId   user being reported
+     * @param message    what the report is about
+     * @return whether or not the report successfully went through
+     * @throws UserNotFoundException user wasn't found
+     * @throws AuthorizationException report is invalid
+     */
+    public boolean reportUser(String fromUserId, String toUserId, String message) throws UserNotFoundException, AuthorizationException {
+        boolean successful = false;
+        if (fromUserId.equals(toUserId)) throw new AuthorizationException("You cannot report yourself.");
+        if (getUser(fromUserId).isFrozen()) throw new AuthorizationException("This user is frozen and can't report others.");
+        Report report = new Report(fromUserId, toUserId, message);
+
+        // Add the report to all admins so that they can see the report.
+        for (User user : getUserDatabase().getItems()) {
+            if (user instanceof Admin) {
+                Admin admin = ((Admin) user);
+                admin.getReports().add(report);
+                updateUserDatabase(admin);
+                successful = true;
+            }
+        }
+        return successful;
+    }
+
+    /**
+     * Gets all reports
+     * @return all reports
+     */
+    public ArrayList<Report> getReports(){
+        for (User user : getUserDatabase().getItems()) {
+            if (user instanceof Admin) {
+                Admin admin = ((Admin) user);
+                return admin.getReports();
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    /**
+     * Set the list of all reports that admins see
+     * @param reports list of all reports
+     */
+    public void setReports(ArrayList<Report> reports){
+        for (User user : getUserDatabase().getItems()) {
+            if (user instanceof Admin) {
+                Admin admin = ((Admin) user);
+                admin.setReports(reports);
+                updateUserDatabase(admin);
+            }
+        }
+    }
+
+    /**
+     * Clears all reports
+     */
+    public void clearReports(){
+        for (User user : getUserDatabase().getItems()) {
+            if (user instanceof Admin) {
+                Admin admin = ((Admin) user);
+                admin.setReports(new ArrayList<>());
+                updateUserDatabase(admin);
+            }
+        }
+    }
+
 }
