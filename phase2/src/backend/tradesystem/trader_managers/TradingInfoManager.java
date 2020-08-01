96ddb0c70a030c15c37f84225c59f8c1637167b6
Andrew Qian <andrew.qian@mail.utoronto.ca>
2020-08-01 15:59:14 -0400

    fixed import

--- phase2/src/backend/tradesystem/trader_managers/TradingInfoManager.java
+++ phase2/src/backend/tradesystem/trader_managers/TradingInfoManager.java
@@ -5,7 +5,7 @@ import backend.models.TradableItem;
 import backend.models.Trade;
 import backend.models.users.Trader;
 import backend.models.users.User;
-import backend.tradesystem.general_managers.Manager;
+import backend.tradesystem.Manager;
 
 import java.io.IOException;
 import java.util.*;
