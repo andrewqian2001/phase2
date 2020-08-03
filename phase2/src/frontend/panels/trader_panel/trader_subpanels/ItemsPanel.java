c250cf2b84c9aea31b769d1835f17e37ffcf7936
James <smurf.lee@mail.utoronto.ca>
2020-08-03 15:26:11 -0400

    item panel javadoc and refactoring

--- phase2/src/frontend/panels/trader_panel/trader_subpanels/ItemsPanel.java
+++ phase2/src/frontend/panels/trader_panel/trader_subpanels/ItemsPanel.java
@@ -9,154 +9,119 @@ import backend.tradesystem.queries.ItemQuery;
 import backend.tradesystem.queries.UserQuery;
 import backend.tradesystem.trader_managers.TraderManager;
 import backend.tradesystem.trader_managers.TradingInfoManager;
-import backend.tradesystem.trader_managers.TradingManager;
 
 import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Shows the list of items and list of wishlist items
+ */
 public class ItemsPanel extends JPanel {
 
-    private String traderId;
-    private Font regular, bold, italic, boldItalic;
+    private final String traderId;
+    private final Font regular, bold;
 
-    private JScrollPane inventoryItemsScrollPane, wishlistItemsScrollPane;
-    private JPanel inventoryTitleContainer, wishlistTitleContainer, inventoryItemsContainer, wishlistItemsContainer;
-    private JButton addInventoryItemButton, addWishlistItemButton;
-    private JLabel inventoryTitle, wishlistTitle;
+    private JPanel inventoryItemsContainer, wishlistItemsContainer;
 
-    private Color bg = new Color(51, 51, 51);
-    private Color gray = new Color(196, 196, 196);
-    private Color gray2 = new Color(142, 142, 142);
-    private Color green = new Color(27, 158, 36);
-    private Color red = new Color(219, 58, 52);
+    private final Color bg = new Color(51, 51, 51);
+    private final Color gray = new Color(196, 196, 196);
+    private final Color gray2 = new Color(142, 142, 142);
+    private final Color green = new Color(27, 158, 36);
+    private final Color red = new Color(219, 58, 52);
 
-    private TradingManager tradeManager;
-    private TraderManager traderManager;
-    private TradingInfoManager infoManager;
+
+    private final TraderManager traderManager = new TraderManager();
+    ;
+    private final TradingInfoManager infoManager = new TradingInfoManager();
+    ;
     private final ItemQuery itemQuery = new ItemQuery();
     private final UserQuery userQuery = new UserQuery();
 
-    public ItemsPanel(String traderId, Font regular, Font bold, Font italic, Font boldItalic) throws IOException {
+    /**
+     * Makes a new panel that shows list of items and wishlist items
+     *
+     * @param traderId   the trader id
+     * @param regular    regular font
+     * @param bold       bold font
+     * @param italic     italics font
+     * @param boldItalic bold italics font
+     * @throws IOException            if issues with accessing database files
+     * @throws UserNotFoundException  trader id is bad
+     * @throws AuthorizationException user isn't a trader or is frozen
+     */
+    public ItemsPanel(String traderId, Font regular, Font bold, Font italic, Font boldItalic) throws IOException, UserNotFoundException, AuthorizationException {
 
         this.traderId = traderId;
         this.regular = regular;
         this.bold = bold;
-        this.italic = italic;
-        this.boldItalic = boldItalic;
 
         this.setSize(1000, 900);
         this.setBackground(bg);
 
-        tradeManager = new TradingManager();
-        traderManager = new TraderManager();
-        infoManager = new TradingInfoManager();
 
-        inventoryTitleContainer = new JPanel(new GridLayout(1, 2));
+        JPanel inventoryTitleContainer = new JPanel(new GridLayout(1, 2));
         inventoryTitleContainer.setPreferredSize(new Dimension(1200, 75));
         inventoryTitleContainer.setBackground(bg);
 
-        inventoryTitle = new JLabel("Inventory");
+        JLabel inventoryTitle = new JLabel("Inventory");
         inventoryTitle.setForeground(Color.WHITE);
         inventoryTitle.setFont(regular.deriveFont(30f));
 
-        addInventoryItemButton = new JButton("Request Item to Inventory");
-        addInventoryItemButton.setForeground(Color.CYAN);
-        addInventoryItemButton.setBackground(bg);
-        addInventoryItemButton.setFont(boldItalic.deriveFont(25f));
-        addInventoryItemButton.setHorizontalAlignment(JButton.RIGHT);
-        addInventoryItemButton.setOpaque(true);
-        addInventoryItemButton.setBorderPainted(false);
-        addInventoryItemButton.addActionListener(e -> {
-            if (traderId.equals("")) return;
-            JDialog addNewItemModal = new JDialog();
-            addNewItemModal.setTitle("Add New Item");
-            addNewItemModal.setSize(500, 500);
-            addNewItemModal.setResizable(false);
-            addNewItemModal.setLocationRelativeTo(null);
+        JButton addInventoryItemButton = addInventoryItemButton(traderId, regular, bold, italic, boldItalic);
 
-            JPanel addNewItemPanel = new JPanel();
-            addNewItemPanel.setPreferredSize(new Dimension(500, 500));
-            addNewItemPanel.setBackground(bg);
-
-            JLabel itemNameTitle = new JLabel("Item Name");
-            itemNameTitle.setFont(italic.deriveFont(20f));
-            itemNameTitle.setPreferredSize(new Dimension(450, 50));
-            itemNameTitle.setOpaque(false);
-            itemNameTitle.setForeground(Color.WHITE);
-
-            JTextField itemNameInput = new JTextField();
-            itemNameInput.setFont(regular.deriveFont(20f));
-            itemNameInput.setBackground(gray2);
-            itemNameInput.setForeground(Color.BLACK);
-            itemNameInput.setPreferredSize(new Dimension(450, 50));
-            itemNameInput.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-            JLabel itemDescTitle = new JLabel("Short Description of Item:");
-            itemDescTitle.setFont(italic.deriveFont(20f));
-            itemDescTitle.setPreferredSize(new Dimension(450, 50));
-            itemDescTitle.setOpaque(false);
-            itemDescTitle.setForeground(Color.WHITE);
-
-            JTextField itemDescInput = new JTextField();
-            itemDescInput.setFont(regular.deriveFont(20f));
-            itemDescInput.setBackground(gray2);
-            itemDescInput.setForeground(Color.BLACK);
-            itemDescInput.setPreferredSize(new Dimension(450, 50));
-            itemDescInput.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-            JButton itemSubmitButton = new JButton("Submit Request");
-            itemSubmitButton.setFont(bold.deriveFont(25f));
-            itemSubmitButton.setBackground(green);
-            itemSubmitButton.setOpaque(true);
-            itemSubmitButton.setForeground(Color.WHITE);
-            itemSubmitButton.setPreferredSize(new Dimension(225, 75));
-            itemSubmitButton.setBorder(BorderFactory.createLineBorder(bg, 15));
-            itemSubmitButton.addActionListener(event -> {
-                if (itemNameInput.getText().trim().length() > 0 && itemDescInput.getText().trim().length() > 0) {
-                    try {
-                        traderManager.addRequestItem(traderId, itemNameInput.getText().trim(), itemDescInput.getText().trim());
-                        addNewItemModal.dispose();
-                    } catch (UserNotFoundException | AuthorizationException e1) {
-                        System.out.println(e1.getMessage());
-                    }
-                }
-            });
-
-            addNewItemPanel.add(itemNameTitle);
-            addNewItemPanel.add(itemNameInput);
-            addNewItemPanel.add(itemDescTitle);
-            addNewItemPanel.add(itemDescInput);
-
-            addNewItemModal.add(addNewItemPanel);
-            addNewItemModal.add(itemSubmitButton, BorderLayout.SOUTH);
-            addNewItemModal.setModal(true);
-            addNewItemModal.setVisible(true);
-        });
-
-        inventoryItemsScrollPane = new JScrollPane();
+        JScrollPane inventoryItemsScrollPane = new JScrollPane();
         inventoryItemsScrollPane.setBorder(null);
         inventoryItemsScrollPane.setPreferredSize(new Dimension(1200, 325));
-        try {
-            getInventory();
-        } catch (UserNotFoundException | AuthorizationException e) {
-            e.printStackTrace();
-        }
+        getInventory();
+
         inventoryItemsScrollPane.setViewportView(inventoryItemsContainer);
 
         JPanel topInventoryItemsScrollHeaderPane = new JPanel(new GridLayout(1, 3));
         topInventoryItemsScrollHeaderPane.setPreferredSize(new Dimension(1200, 50));
 
-        wishlistTitleContainer = new JPanel(new GridLayout(1, 2));
+        JPanel wishlistTitleContainer = new JPanel(new GridLayout(1, 2));
         wishlistTitleContainer.setPreferredSize(new Dimension(1200, 75));
         wishlistTitleContainer.setBackground(bg);
 
-        wishlistTitle = new JLabel("Wishlist");
+        JLabel wishlistTitle = new JLabel("Wishlist");
         wishlistTitle.setForeground(Color.WHITE);
         wishlistTitle.setFont(regular.deriveFont(30f));
 
-        addWishlistItemButton = new JButton("Add Item to Wishlist");
+        JButton addWishlistItemButton = addWishlistItemButton(traderId, regular, bold, italic, boldItalic);
+
+        JScrollPane wishlistItemsScrollPane = new JScrollPane();
+        wishlistItemsScrollPane.setBorder(null);
+        wishlistItemsScrollPane.setPreferredSize(new Dimension(1200, 325));
+        getWishlist();
+        wishlistItemsScrollPane.setViewportView(wishlistItemsContainer);
+
+        JPanel topWishlistItemsScrollHeaderPane = new JPanel(new GridLayout(1, 3));
+        topWishlistItemsScrollHeaderPane.setPreferredSize(new Dimension(1200, 325));
+
+        //JPanel topWishlistItemsScrollHeaderPane = new JPanel(new GridLayout(1, 3));
+        //topWishlistItemsScrollHeaderPane.setPreferredSize(new Dimension(1200, 75));
+
+        inventoryTitleContainer.add(inventoryTitle);
+        inventoryTitleContainer.add(addInventoryItemButton);
+
+        wishlistTitleContainer.add(wishlistTitle);
+        wishlistTitleContainer.add(addWishlistItemButton);
+
+        // topInventoryItemsScrollHeaderPane.add(inventoryItemsScrollPane);
+        // topWishlistItemsScrollHeaderPane.add(wishlistItemsScrollPane);
+
+        this.add(inventoryTitleContainer);
+        this.add(inventoryItemsScrollPane);
+        // this.add(topInventoryItemsScrollHeaderPane);
+        this.add(wishlistTitleContainer);
+        // this.add(topWishlistItemsScrollHeaderPane);
+        this.add(wishlistItemsScrollPane);
+    }
+
+    private JButton addWishlistItemButton(String traderId, Font regular, Font bold, Font italic, Font boldItalic) {
+        JButton addWishlistItemButton = new JButton("Add Item to Wishlist");
         addWishlistItemButton.setForeground(Color.CYAN);
         addWishlistItemButton.setBackground(bg);
         addWishlistItemButton.setFont(boldItalic.deriveFont(25f));
@@ -247,42 +212,88 @@ public class ItemsPanel extends JPanel {
             addNewItemModal.setModal(true);
             addNewItemModal.setVisible(true);
         });
+        return addWishlistItemButton;
+    }
 
-        wishlistItemsScrollPane = new JScrollPane();
-        wishlistItemsScrollPane.setBorder(null);
-        wishlistItemsScrollPane.setPreferredSize(new Dimension(1200, 325));
-        try {
-            getWishlist();
-        } catch (UserNotFoundException | AuthorizationException e) {
-            e.printStackTrace();
-        }
-        wishlistItemsScrollPane.setViewportView(wishlistItemsContainer);
+    private JButton addInventoryItemButton(String traderId, Font regular, Font bold, Font italic, Font boldItalic) {
+        JButton addInventoryItemButton = new JButton("Request Item to Inventory");
+        addInventoryItemButton.setForeground(Color.CYAN);
+        addInventoryItemButton.setBackground(bg);
+        addInventoryItemButton.setFont(boldItalic.deriveFont(25f));
+        addInventoryItemButton.setHorizontalAlignment(JButton.RIGHT);
+        addInventoryItemButton.setOpaque(true);
+        addInventoryItemButton.setBorderPainted(false);
+        addInventoryItemButton.addActionListener(e -> {
+            if (traderId.equals("")) return;
+            JDialog addNewItemModal = new JDialog();
+            addNewItemModal.setTitle("Add New Item");
+            addNewItemModal.setSize(500, 500);
+            addNewItemModal.setResizable(false);
+            addNewItemModal.setLocationRelativeTo(null);
 
-        JPanel topWishlistItemsScrollHeaderPane = new JPanel(new GridLayout(1, 3));
-        topWishlistItemsScrollHeaderPane.setPreferredSize(new Dimension(1200, 325));
+            JPanel addNewItemPanel = new JPanel();
+            addNewItemPanel.setPreferredSize(new Dimension(500, 500));
+            addNewItemPanel.setBackground(bg);
 
-        //JPanel topWishlistItemsScrollHeaderPane = new JPanel(new GridLayout(1, 3));
-        //topWishlistItemsScrollHeaderPane.setPreferredSize(new Dimension(1200, 75));
+            JLabel itemNameTitle = new JLabel("Item Name");
+            itemNameTitle.setFont(italic.deriveFont(20f));
+            itemNameTitle.setPreferredSize(new Dimension(450, 50));
+            itemNameTitle.setOpaque(false);
+            itemNameTitle.setForeground(Color.WHITE);
 
-        inventoryTitleContainer.add(inventoryTitle);
-        inventoryTitleContainer.add(addInventoryItemButton);
+            JTextField itemNameInput = new JTextField();
+            itemNameInput.setFont(regular.deriveFont(20f));
+            itemNameInput.setBackground(gray2);
+            itemNameInput.setForeground(Color.BLACK);
+            itemNameInput.setPreferredSize(new Dimension(450, 50));
+            itemNameInput.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        wishlistTitleContainer.add(wishlistTitle);
-        wishlistTitleContainer.add(addWishlistItemButton);
+            JLabel itemDescTitle = new JLabel("Short Description of Item:");
+            itemDescTitle.setFont(italic.deriveFont(20f));
+            itemDescTitle.setPreferredSize(new Dimension(450, 50));
+            itemDescTitle.setOpaque(false);
+            itemDescTitle.setForeground(Color.WHITE);
 
-        // topInventoryItemsScrollHeaderPane.add(inventoryItemsScrollPane);
-        // topWishlistItemsScrollHeaderPane.add(wishlistItemsScrollPane);
+            JTextField itemDescInput = new JTextField();
+            itemDescInput.setFont(regular.deriveFont(20f));
+            itemDescInput.setBackground(gray2);
+            itemDescInput.setForeground(Color.BLACK);
+            itemDescInput.setPreferredSize(new Dimension(450, 50));
+            itemDescInput.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        this.add(inventoryTitleContainer);
-        this.add(inventoryItemsScrollPane);
-        // this.add(topInventoryItemsScrollHeaderPane);
-        this.add(wishlistTitleContainer);
-        // this.add(topWishlistItemsScrollHeaderPane);
-        this.add(wishlistItemsScrollPane);
+            JButton itemSubmitButton = new JButton("Submit Request");
+            itemSubmitButton.setFont(bold.deriveFont(25f));
+            itemSubmitButton.setBackground(green);
+            itemSubmitButton.setOpaque(true);
+            itemSubmitButton.setForeground(Color.WHITE);
+            itemSubmitButton.setPreferredSize(new Dimension(225, 75));
+            itemSubmitButton.setBorder(BorderFactory.createLineBorder(bg, 15));
+            itemSubmitButton.addActionListener(event -> {
+                if (itemNameInput.getText().trim().length() > 0 && itemDescInput.getText().trim().length() > 0) {
+                    try {
+                        traderManager.addRequestItem(traderId, itemNameInput.getText().trim(), itemDescInput.getText().trim());
+                        addNewItemModal.dispose();
+                    } catch (UserNotFoundException | AuthorizationException e1) {
+                        System.out.println(e1.getMessage());
+                    }
+                }
+            });
+
+            addNewItemPanel.add(itemNameTitle);
+            addNewItemPanel.add(itemNameInput);
+            addNewItemPanel.add(itemDescTitle);
+            addNewItemPanel.add(itemDescInput);
+
+            addNewItemModal.add(addNewItemPanel);
+            addNewItemModal.add(itemSubmitButton, BorderLayout.SOUTH);
+            addNewItemModal.setModal(true);
+            addNewItemModal.setVisible(true);
+        });
+        return addInventoryItemButton;
     }
 
     private void getInventory() throws UserNotFoundException, AuthorizationException {
-        ArrayList<String> availableItems = traderId.equals("") ? new ArrayList<>(): userQuery.getAvailableItems(traderId);
+        ArrayList<String> availableItems = traderId.equals("") ? new ArrayList<>() : userQuery.getAvailableItems(traderId);
         int numRows = availableItems.size();
         if (numRows < 4) numRows = 4;
         inventoryItemsContainer = new JPanel(new GridLayout(numRows, 1));
@@ -343,7 +354,7 @@ public class ItemsPanel extends JPanel {
     }
 
     private void getWishlist() throws UserNotFoundException, AuthorizationException {
-        ArrayList<String> wishlist = traderId.equals("") ? new ArrayList<>(): userQuery.getWishlist(traderId);
+        ArrayList<String> wishlist = traderId.equals("") ? new ArrayList<>() : userQuery.getWishlist(traderId);
         int numRows = wishlist.size();
         if (numRows < 4) numRows = 4;
         wishlistItemsContainer = new JPanel(new GridLayout(numRows, 1));
