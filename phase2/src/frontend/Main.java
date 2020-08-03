6ce455fdc5a54102dc2519591493974bc34926b8
James <smurf.lee@mail.utoronto.ca>
2020-08-03 19:03:43 -0400

    get all users

--- phase2/src/frontend/Main.java
+++ phase2/src/frontend/Main.java
@@ -5,10 +5,10 @@ import javax.swing.UIManager;
 /**
  * This is where the program starts running
  */
-public class Main {
+    public class Main {
     public static void main(String[] args) {
         try {
-            //   new TemporarySetup();
+               new TemporarySetup();
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
             WindowManager windowManager = new WindowManager();
             windowManager.run();
