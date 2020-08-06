f87f98f2946ffaec858dcf9623e66cf259309270
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-08-05 20:30:28 -0400

    addNewTradeModal organization done, functionality not implemented yet

--- phase2/src/frontend/panels/trader_panel/trader_subpanels/trade_panels/trade_modals/AddNewTradeModal.java
+++ phase2/src/frontend/panels/trader_panel/trader_subpanels/trade_panels/trade_modals/AddNewTradeModal.java
@@ -1,65 +1,428 @@
 package frontend.panels.trader_panel.trader_subpanels.trade_panels.trade_modals;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class AddNewTradeModal extends JDialog {
+import backend.exceptions.AuthorizationException;
+import backend.exceptions.TradableItemNotFoundException;
+import backend.exceptions.TradeNotFoundException;
+import backend.exceptions.UserNotFoundException;
+import backend.tradesystem.queries.ItemQuery;
+import backend.tradesystem.queries.TradeQuery;
+import backend.tradesystem.queries.UserQuery;
+import backend.tradesystem.trader_managers.TradingInfoManager;
+import backend.tradesystem.trader_managers.TradingManager;
 
-	public AddNewTradeModal(String trader, String[] suggested, Font regular, Font bold, Font italic, Font boldItalic) {
+public class AddNewTradeModal extends JDialog implements ActionListener {
+
+    private String trader;
+    private String[] suggested;
+
+    private final Font regular, bold, italic, boldItalic;
+
+    private final Color bg = new Color(51, 51, 51);
+    private final Color gray = new Color(196, 196, 196);
+    private final Color gray2 = new Color(142, 142, 142);
+    private final Color green = new Color(27, 158, 36);
+    private final Color red = new Color(219, 58, 52);
+
+    private final TradeQuery tradeQuery = new TradeQuery();
+    private final UserQuery userQuery = new UserQuery();
+    private final ItemQuery itemQuery = new ItemQuery();
+
+    private final TradingManager tradeManager = new TradingManager();
+    private final TradingInfoManager infoManager = new TradingInfoManager();
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd yyyy HH:mm", new Locale("en", "US"));
+
+	public AddNewTradeModal(String trader, String[] suggested, Font regular, Font bold, Font italic, Font boldItalic) throws IOException {
+
+        this.trader = trader;
+        this.suggested = suggested;
+        this.regular = regular;
+        this.bold = bold;
+        this.italic = italic;
+        this.boldItalic = boldItalic;
+
+        this.setTitle("Add New Trade");
+        this.setSize(500, 1200);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
+        boolean isSuggested = suggested.length == 0;
+
+        JPanel addNewTradePanel = setAddNewTradePanel(isSuggested);
+
+        JButton tradeSubmitButton = setTradeSubmitButton();
+
+        this.add(addNewTradePanel);
+        this.add(tradeSubmitButton, BorderLayout.SOUTH);
+        this.setModal(true);
 	}
-    
-}
-/*
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (trader.equals(""))
-            return;
+
+    private JButton setTradeSubmitButton() {
+        JButton tradeSubmitButton = new JButton("Submit");
+        tradeSubmitButton.setFont(bold.deriveFont(25f));
+        tradeSubmitButton.setBackground(green);
+        tradeSubmitButton.setOpaque(true);
+        tradeSubmitButton.setForeground(Color.WHITE);
+        tradeSubmitButton.setPreferredSize(new Dimension(225, 75));
+        tradeSubmitButton.setBorder(BorderFactory.createLineBorder(bg, 15));
+        tradeSubmitButton.addActionListener(this);
+
+        return tradeSubmitButton;
+    }
+
+    private JPanel setTradeWithinCityPanel() {
+        JPanel tradeWithinCityPanel = new JPanel();
+        tradeWithinCityPanel.setBackground(bg);
+        tradeWithinCityPanel.setPreferredSize(new Dimension(500, 50));
+
+        JLabel tradeWithinCityTitle = new JLabel("Trade Within City?");
+        tradeWithinCityTitle.setFont(italic.deriveFont(20f));
+        tradeWithinCityTitle.setPreferredSize(new Dimension(425, 50));
+        tradeWithinCityTitle.setOpaque(false);
+        tradeWithinCityTitle.setForeground(Color.WHITE);
+
+        JCheckBox tradeWithinCityButton = new JCheckBox();
+        tradeWithinCityButton.setPreferredSize(new Dimension(25, 25));
+        tradeWithinCityButton.setSelected(false);
+        tradeWithinCityButton.setForeground(Color.WHITE);
+        tradeWithinCityButton.setBackground(bg);
+
+        tradeWithinCityPanel.add(tradeWithinCityTitle);
+        tradeWithinCityPanel.add(tradeWithinCityButton);
+
+        return tradeWithinCityPanel;
+    }
+
+    private JPanel setOtherTradersPanel() {
 
         ArrayList<String> allTraders = infoManager.getAllTraders();
 
-        boolean isSuggestedTrade = e.getActionCommand().equals("<html><b><i><u>Suggest Trade</u></i></b></html>");
-        boolean isSuggestedLend = e.getActionCommand().equals("<html><b><i><u>Suggest Lend</u></i></b></html>");
+        JPanel otherTradersPanel = new JPanel();
+        otherTradersPanel.setBackground(bg);
+        otherTradersPanel.setPreferredSize(new Dimension(500, 125));
 
-        String[] suggested = new String[0];
-        try {
-            if (isSuggestedLend)
-                suggested = infoManager.suggestLend(trader, false);
-            else if (isSuggestedTrade) {
-                suggested = infoManager.suggestTrade(trader, false);
-                if(suggested.length == 0) {
-                    suggested = infoManager.automatedTradeSuggestion(trader, true);
+        JLabel otherTraderNameTitle = new JLabel("Trader Username:");
+        otherTraderNameTitle.setFont(italic.deriveFont(20f));
+        otherTraderNameTitle.setPreferredSize(new Dimension(450, 50));
+        otherTraderNameTitle.setOpaque(false);
+        otherTraderNameTitle.setForeground(Color.WHITE);
+
+        JComboBox<String> traders = new JComboBox<>();
+        traders.setPreferredSize(new Dimension(450, 50));
+        traders.setFont(regular.deriveFont(20f));
+        traders.setBackground(gray2);
+        traders.setForeground(Color.BLACK);
+        traders.setOpaque(true);
+        traders.addItem(null);
+        allTraders.forEach(traderId -> {
+            if (!traderId.equals(trader)) {
+                try {
+                    traders.addItem(userQuery.getUsername(traderId));
+                } catch (UserNotFoundException e2) {
+                    e2.printStackTrace();
                 }
             }
-        } catch (UserNotFoundException | AuthorizationException e3) {
-            e3.printStackTrace();
+        });
+
+        otherTradersPanel.add(otherTraderNameTitle);
+        otherTradersPanel.add(traders);
+        return otherTradersPanel;
+    }
+
+    private JPanel setTraderItemsPanel() {
+
+        JPanel traderItemsPanel = new JPanel();
+        traderItemsPanel.setBackground(bg);
+        traderItemsPanel.setPreferredSize(new Dimension(500, 125));
+
+        JLabel traderItemTitle = new JLabel("Item from your Inventory:");
+        traderItemTitle.setFont(italic.deriveFont(20f));
+        traderItemTitle.setPreferredSize(new Dimension(450, 50));
+        traderItemTitle.setOpaque(false);
+        traderItemTitle.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        traderItemTitle.setForeground(Color.WHITE);
+
+        JComboBox<String> traderItems = new JComboBox<>();
+        traderItems.setFont(regular.deriveFont(20f));
+        traderItems.setBackground(gray2);
+        traderItems.setForeground(Color.BLACK);
+        traderItems.setOpaque(true);
+        traderItems.setPreferredSize(new Dimension(450, 50));
+        try {
+            for (String itemId : userQuery.getAvailableItems(trader)) {
+                traderItems.addItem(itemQuery.getName(itemId));
+            }
+        } catch (AuthorizationException | UserNotFoundException | TradableItemNotFoundException exception) {
+            exception.printStackTrace();
         }
+        traderItems.addItem(null);
 
-        if (suggested.length == 0 && (isSuggestedLend || isSuggestedTrade)) {
-            isSuggestedTrade = false;
-            isSuggestedLend = false;
+        traderItemsPanel.add(traderItemTitle);
+        traderItemsPanel.add(traderItems);
 
-            JDialog noSuggestionsFound = new JDialog();
-            noSuggestionsFound.setTitle("No Suggestions Found");
-            noSuggestionsFound.setSize(500, 200);
-            noSuggestionsFound.setResizable(false);
-            noSuggestionsFound.setLocationRelativeTo(null);
+        return traderItemsPanel;
+    }
 
-            JTextArea noSuggestionsTitle = new JTextArea("Unfortunately, we are not able to find a trade suggestion for you.\n\nClosing this pop-up will take you to the\n'Add New Trade' menu.");
-            noSuggestionsTitle.setFont(regular.deriveFont(22f));
-            noSuggestionsTitle.setBackground(bg);
-            noSuggestionsTitle.setForeground(Color.WHITE);
-            noSuggestionsTitle.setPreferredSize(new Dimension(500,200));
-            noSuggestionsTitle.setOpaque(true);
-            noSuggestionsTitle.setEditable(false);
-            noSuggestionsTitle.setLineWrap(true);
-            noSuggestionsTitle.setWrapStyleWord(true);
-            noSuggestionsTitle.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    private JPanel setOtherTraderItemsPanel() {
 
-            noSuggestionsFound.add(noSuggestionsTitle);
-            noSuggestionsFound.setModal(true);
-            noSuggestionsFound.setVisible(true);
-        }
+        JPanel otherTraderItemsPanel = new JPanel();
+        otherTraderItemsPanel.setBackground(bg);
+        otherTraderItemsPanel.setPreferredSize(new Dimension(500, 125));
+
+        JLabel otherTraderItemsTitle = new JLabel("Item from their Inventory:");
+        otherTraderItemsTitle.setFont(italic.deriveFont(20f));
+        otherTraderItemsTitle.setPreferredSize(new Dimension(450, 50));
+        otherTraderItemsTitle.setOpaque(false);
+        otherTraderItemsTitle.setForeground(Color.WHITE);
+
+        JComboBox<String> otherTraderItems = new JComboBox<>();
+        otherTraderItems.setPreferredSize(new Dimension(450, 50));
+        otherTraderItems.setFont(regular.deriveFont(20f));
+        otherTraderItems.setBackground(gray2);
+        otherTraderItems.setForeground(Color.BLACK);
+        otherTraderItems.setOpaque(true);
+        otherTraderItems.setEnabled(false);
+
+        otherTraderItemsPanel.add(otherTraderItemsTitle);
+        otherTraderItemsPanel.add(otherTraderItems);
+
+        return otherTraderItemsPanel;
+    }
+
+    private JPanel setMeetingLocationPanel() {
+        JPanel meetingLocationPanel = new JPanel();
+        meetingLocationPanel.setBackground(bg);
+        meetingLocationPanel.setPreferredSize(new Dimension(500, 125));
+
+        JLabel meetingLocationTitle = new JLabel("Meeting Location");
+        meetingLocationTitle.setFont(italic.deriveFont(20f));
+        meetingLocationTitle.setPreferredSize(new Dimension(450, 50));
+        meetingLocationTitle.setOpaque(false);
+        meetingLocationTitle.setForeground(Color.WHITE);
+
+        JTextField meetingLocationInput = new JTextField();
+        meetingLocationInput.setFont(regular.deriveFont(20f));
+        meetingLocationInput.setBackground(gray2);
+        meetingLocationInput.setForeground(Color.BLACK);
+        meetingLocationInput.setPreferredSize(new Dimension(450, 50));
+        meetingLocationInput.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        meetingLocationPanel.add(meetingLocationTitle);
+        meetingLocationPanel.add(meetingLocationInput);
+
+        return meetingLocationPanel;
+    }
+
+    private JPanel setIsTemporaryPanel() {
+        JPanel isTemporaryPanel = new JPanel();
+        isTemporaryPanel.setBackground(bg);
+        isTemporaryPanel.setPreferredSize(new Dimension(500, 50));
+
+        JLabel isTemporaryTitle = new JLabel("Is this trade temporary?");
+        isTemporaryTitle.setFont(italic.deriveFont(20f));
+        isTemporaryTitle.setPreferredSize(new Dimension(425, 50));
+        isTemporaryTitle.setOpaque(false);
+        isTemporaryTitle.setForeground(Color.WHITE);
+
+        JCheckBox isTemporaryButton = new JCheckBox();
+        isTemporaryButton.setPreferredSize(new Dimension(25, 25));
+        isTemporaryButton.setSelected(true);
+        isTemporaryButton.setForeground(Color.WHITE);
+        isTemporaryButton.setBackground(bg);
+
+        isTemporaryPanel.add(isTemporaryTitle);
+        isTemporaryPanel.add(isTemporaryButton);
+        
+        return isTemporaryPanel;
+    }
+
+    private JPanel setMeetingsPanel() {
+        JPanel meetingsPanel = new JPanel();
+        meetingsPanel.setBackground(bg);
+        meetingsPanel.setPreferredSize(new Dimension(450, 250));
+
+        JLabel firstMeetingDateTitle = new JLabel("First Meeting Date:");
+        firstMeetingDateTitle.setPreferredSize(new Dimension(450, 50));
+        firstMeetingDateTitle.setFont(italic.deriveFont(20f));
+        firstMeetingDateTitle.setOpaque(false);
+        firstMeetingDateTitle.setForeground(Color.WHITE);
+
+        JPanel firstMeetingDate = dateInput();
+
+        JLabel secondMeetingDateTitle = new JLabel("Second Meeting Date:");
+        secondMeetingDateTitle.setPreferredSize(new Dimension(450, 50));
+        secondMeetingDateTitle.setFont(italic.deriveFont(20f));
+        secondMeetingDateTitle.setOpaque(false);
+        secondMeetingDateTitle.setForeground(Color.WHITE);
+
+        JPanel secondMeetingDate = dateInput();
+
+        // isTemporaryButton.addItemListener(ex -> {
+            // if (isTemporaryButton.isSelected()) {
+                // secondMeetingDateTitle.setVisible(true);
+                // secondMeetingDate.setVisible(true);
+            // } else {
+                // secondMeetingDateTitle.setVisible(false);
+                // secondMeetingDate.setVisible(false);
+            // }
+        // });
+
+        meetingsPanel.add(firstMeetingDateTitle);
+        meetingsPanel.add(firstMeetingDate);
+        meetingsPanel.add(secondMeetingDateTitle);
+        meetingsPanel.add(secondMeetingDate);
+
+        return meetingsPanel;
+    }
+
+    private JPanel setMessagePanel() {
+        JPanel messagePanel = new JPanel();
+        messagePanel.setBackground(bg);
+        messagePanel.setPreferredSize(new Dimension(500, 125));
+
+        JLabel messageTitle = new JLabel("Attach a message with this trade: (Optional)");
+        messageTitle.setFont(italic.deriveFont(20f));
+        messageTitle.setPreferredSize(new Dimension(450, 50));
+        messageTitle.setOpaque(false);
+        messageTitle.setForeground(Color.WHITE);
+
+        JTextField messageInput = new JTextField();
+        messageInput.setFont(regular.deriveFont(20f));
+        messageInput.setBackground(gray2);
+        messageInput.setForeground(Color.BLACK);
+        messageInput.setPreferredSize(new Dimension(450, 50));
+        messageInput.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        messagePanel.add(messageTitle);
+        messagePanel.add(messageInput);
+
+        return messagePanel;
+    }
+
+    private JPanel setAddNewTradePanel(boolean isSuggested) {
+        JPanel addNewTradePanel = new JPanel();
+        addNewTradePanel.setPreferredSize(new Dimension(500, 1050));
+        addNewTradePanel.setBackground(bg);
+        
+        JPanel tradeWithinCityPanel = setTradeWithinCityPanel();
+        JPanel otherTradersPanel = setOtherTradersPanel();
+        JPanel traderItemsPanel = setTraderItemsPanel();
+        JPanel otherTraderItemsPanel = setOtherTraderItemsPanel();
+        JPanel meetingLocationPanel = setMeetingLocationPanel();
+        JPanel isTemporaryPanel = setIsTemporaryPanel();
+        JPanel meetingsPanel = setMeetingsPanel();
+        JPanel messagePanel = setMessagePanel();
+
+        JLabel error = new JLabel();
+        error.setPreferredSize(new Dimension(500, 50));
+        error.setForeground(red);
+        error.setFont(boldItalic.deriveFont(20f));
+        error.setHorizontalAlignment(JLabel.CENTER);
+        error.setVisible(false);
+
+        addNewTradePanel.add(tradeWithinCityPanel);
+        addNewTradePanel.add(otherTradersPanel);
+        addNewTradePanel.add(traderItemsPanel);
+        addNewTradePanel.add(otherTraderItemsPanel);
+        addNewTradePanel.add(meetingLocationPanel);
+        addNewTradePanel.add(isTemporaryPanel);
+        addNewTradePanel.add(meetingsPanel);
+        addNewTradePanel.add(messagePanel);
+        addNewTradePanel.add(error);
+
+        return addNewTradePanel;
+    }
+
+    private JPanel dateInput() {
+        JPanel meetingInput = new JPanel();
+        meetingInput.setBackground(bg);
+        meetingInput.setPreferredSize(new Dimension(450, 50));
+
+        String[] monthList = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        JComboBox<String> months = new JComboBox<>(monthList);
+        months.setPreferredSize(new Dimension(100, 50));
+        months.setFont(regular.deriveFont(20f));
+
+        JComboBox<Integer> days = new JComboBox<>();
+        days.setFont(regular.deriveFont(20f));
+        days.setPreferredSize(new Dimension(60, 50));
+        for (int i = 1; i < 32; i++)
+            days.addItem(i);
+
+        months.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                int numDays = 0;
+                if (e.getItem().equals("Apr") || e.getItem().equals("Jun") || e.getItem().equals("Sep")
+                        || e.getItem().equals("Nov"))
+                    numDays = 30;
+                else if (e.getItem().equals("Feb"))
+                    numDays = 28;
+                else
+                    numDays = 31;
+                days.removeAllItems();
+                for (int i = 1; i <= numDays; i++)
+                    days.addItem(i);
+            }
+        });
+
+        JComboBox<Integer> years = new JComboBox<>();
+        years.setPreferredSize(new Dimension(100, 50));
+        years.setFont(regular.deriveFont(20f));
+        for (int i = 2020; i < 2026; i++)
+            years.addItem(i);
+
+        JComboBox<Integer> hours = new JComboBox<>();
+        hours.setPreferredSize(new Dimension(50, 50));
+        hours.setFont(regular.deriveFont(20f));
+        for (int i = 0; i < 24; i++)
+            hours.addItem(i);
+
+        JComboBox<Integer> minutes = new JComboBox<>();
+        minutes.setPreferredSize(new Dimension(50, 50));
+        minutes.setFont(regular.deriveFont(20f));
+        for (int i = 0; i < 60; i++)
+            minutes.addItem(i);
+
+        meetingInput.add(months);
+        meetingInput.add(days);
+        meetingInput.add(years);
+        meetingInput.add(hours);
+        meetingInput.add(minutes);
+        return meetingInput;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.dispose();
+    }
+    
+}
+/*
 
         JDialog addNewTradeModal = new JDialog();
         addNewTradeModal.setTitle("Add New Trade");
