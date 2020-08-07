6a1f76f9aff199f2ef4f22ac2c09c33de3200c0c
truanov1 <ilancito7@gmail.com>
2020-08-06 23:21:38 -0400

    Suggestion javadoc

--- phase2/src/backend/models/Suggestion.java
+++ phase2/src/backend/models/Suggestion.java
@@ -1,5 +1,8 @@
 package backend.models;
 
+/**
+ * This class represents a suggestion for a trade
+ */
 public class Suggestion {
 
     private final String FROM_TRADER_ID;
@@ -8,6 +11,13 @@ public class Suggestion {
     private final String TO_TRADER_OFFER_ID;
 
 
+    /**
+     * Constructs a trade suggestion
+     * @param fromTraderId the id of the trader who will offer this suggested trade
+     * @param toTraderId the id of the trader who will be sent this suggested trade
+     * @param fromTraderOfferId the id of the item of the trader that will offer this suggested trade
+     * @param toTraderOfferId the id of the item of the trader who will be sent this suggested trade
+     */
     public Suggestion(String fromTraderId, String toTraderId, String fromTraderOfferId, String toTraderOfferId){
         this.FROM_TRADER_ID = fromTraderId;
         this.TO_TRADER_ID = toTraderId;
@@ -15,22 +25,44 @@ public class Suggestion {
         this.TO_TRADER_OFFER_ID = toTraderOfferId;
     }
 
+    /**
+     * Constructs a lend suggestion
+     * @param fromTraderId the id of the trader who will offer this suggested trade
+     * @param toTraderId the id of the trader who will be sent this suggested trade
+     * @param fromTraderOfferId the id of the item of the trader that will offer this suggested trade
+     */
     public Suggestion(String fromTraderId, String toTraderId, String fromTraderOfferId){
         this(fromTraderId, toTraderId, fromTraderOfferId, "");
     }
 
+    /**
+     * Get the id of the trader who will offer this suggested trade
+     * @return the id of the trader who will offer this suggested trade
+     */
     public String getFromTraderId() {
         return FROM_TRADER_ID;
     }
 
+    /**
+     * Get the id of the item of the trader that will offer this suggested trade
+     * @return the id of the item of the trader that will offer this suggested trade
+     */
     public String getFromTraderOfferId() {
         return FROM_TRADER_OFFER_ID;
     }
 
+    /**
+     * Get the id of the trader who will be sent this suggested trade
+     * @return the id of the trader who will be sent this suggested trade
+     */
     public String getToTraderId() {
         return TO_TRADER_ID;
     }
 
+    /**
+     * Get the id of the item of the trader who will be sent this suggested trade
+     * @return the id of the item of the trader who will be sent this suggested trade
+     */
     public String getToTraderOfferId() {
         return TO_TRADER_OFFER_ID;
     }
