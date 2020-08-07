34826f3d2192af8cd0174213a5b4df2b0675a8f7
truanov1 <ilancito7@gmail.com>
2020-08-07 13:14:58 -0400

    Strategy interfaces javadoc

--- phase2/src/backend/tradesystem/suggestion_strategies/SuggestLendStrategy.java
+++ phase2/src/backend/tradesystem/suggestion_strategies/SuggestLendStrategy.java
@@ -3,8 +3,19 @@ package backend.tradesystem.suggestion_strategies;
 import backend.exceptions.AuthorizationException;
 import backend.exceptions.UserNotFoundException;
 
+/**
+ * Interface which has a suggestLend method that represents a suggestLend algorithm
+ */
 public interface SuggestLendStrategy {
 
+    /**
+     * Suggests a lend where thisTraderId is lending some item.
+     * @param thisTraderId the id of the trader asking for the suggestion
+     * @param inCity true if you desire to search for other traders within the same city as the original trader
+     * @return A lend suggestion
+     * @throws UserNotFoundException if a user was not found
+     * @throws AuthorizationException if the given trader id represents a non-trader object.
+     */
     Suggestion suggestLend(String thisTraderId, boolean inCity) throws UserNotFoundException, AuthorizationException;
 
 }
--- phase2/src/backend/tradesystem/suggestion_strategies/SuggestTradeStrategy.java
+++ phase2/src/backend/tradesystem/suggestion_strategies/SuggestTradeStrategy.java
@@ -3,7 +3,18 @@ package backend.tradesystem.suggestion_strategies;
 import backend.exceptions.AuthorizationException;
 import backend.exceptions.UserNotFoundException;
 
+/**
+ * Interface which has a suggestTrade method that represents a suggestTrade algorithm
+ */
 public interface SuggestTradeStrategy {
 
+    /**
+     * Suggests a trade where thisTraderId is offering some item to another trader.
+     * @param thisTraderId the id of the trader asking for the suggestion
+     * @param inCity true if you desire to search for other traders within the same city as the original trader
+     * @return A trade suggestion
+     * @throws UserNotFoundException if a user was not found
+     * @throws AuthorizationException if the given trader id represents a non-trader object.
+     */
     Suggestion suggestTrade(String thisTraderId, boolean inCity) throws UserNotFoundException, AuthorizationException;
 }
